b49eae9cdfd9c1492917d448232c79478bbeede1
syzygy333 <brianjonashogue@gmail.com>
2017-03-15 13:56:52 -0400

    Add plugin file and start it off with comment

---
+++ keeler-plot-generator.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Keeler Plot Generator
+ * Plugin URI: https://github.com/syzygy333
+ * Description: This plugin is intended for use by the website of the Harry Stephen Keeler Society. It generates a plotline typical of a Keeler novel with randomized bits to make it different on each pageload.
+ * Version: 1.0.0
+ * Author: Brian Hogue (plugin) and Richard Polt (JS version of generator)
+ * Author URI: https://github.com/syzygy333
+ * License: GPL2
+ */
